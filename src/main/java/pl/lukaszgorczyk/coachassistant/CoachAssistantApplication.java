1b1c21ed3946bdc2e0286709c173a0156c444504
Lukasz Gorczyk <lukasz.gorczyk.programista@gmail.com>
2022-05-08 23:29:25 +0200

    Project start

---
+++ src/main/java/pl/lukaszgorczyk/coachassistant/CoachAssistantApplication.java
@@ -0,0 +1,13 @@
+package pl.lukaszgorczyk.coachassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lukaszgorczyk/coachassistant/CoachAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lukaszgorczyk.coachassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
